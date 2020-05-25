551f910d699479ee47dfa8253134896e0b6ae6f7
smasiu <szymon.masko.dev@gmail.com>
2020-05-25 14:23:29 +0200

    Updates

---
+++ precache-manifest.5470f76f740831134ef317f22caa059e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e61459018808f8cd82636acfb7f1758c",
+    "url": "/SMasiu.hithub.io/book-store/index.html"
+  },
+  {
+    "revision": "625e81136c1b52f3fc3e",
+    "url": "/SMasiu.hithub.io/book-store/static/css/main.27304420.chunk.css"
+  },
+  {
+    "revision": "63a412e86a60410dc675",
+    "url": "/SMasiu.hithub.io/book-store/static/js/2.83d4022d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/SMasiu.hithub.io/book-store/static/js/2.83d4022d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "625e81136c1b52f3fc3e",
+    "url": "/SMasiu.hithub.io/book-store/static/js/main.ddd4214e.chunk.js"
+  },
+  {
+    "revision": "85410aa9b5cd8a64e621",
+    "url": "/SMasiu.hithub.io/book-store/static/js/runtime-main.14e38ded.js"
+  }
+]);
