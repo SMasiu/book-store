9bd42c176f389c04b0a5078dcd84835213cffcfd
smasiu <szymon.masko.dev@gmail.com>
2020-05-25 14:13:25 +0200

    build

---
+++ build/precache-manifest.b80c83469f019aa65e7c0b90c41db2e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e451b3718e1f1616e6ff95d4c0a5d5bd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1f405b8b3013e88bee80",
+    "url": "/static/css/main.27304420.chunk.css"
+  },
+  {
+    "revision": "63a412e86a60410dc675",
+    "url": "/static/js/2.83d4022d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.83d4022d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f405b8b3013e88bee80",
+    "url": "/static/js/main.ed8c4d12.chunk.js"
+  },
+  {
+    "revision": "63ba08987322a76962dc",
+    "url": "/static/js/runtime-main.ec26715b.js"
+  }
+]);
