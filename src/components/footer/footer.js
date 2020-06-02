7304435cdf1a8555e7e5abd415a0d64e9f4718db
smasiu <szymon.masko.dev@gmail.com>
2020-06-02 10:49:54 +0200

    SEO

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,50 +1,65 @@
-import React from 'react';
-import './footer.css';
-import List from '../list/list';
-import Button from '../button/button';
+import React from "react";
+import "./footer.css";
+import List from "../list/list";
+import Button from "../button/button";
 
 const Footer = () => (
-    <footer className="main-footer" id="contact-main">
-        <section className="footer-content">
-            <h2>Conact</h2>
+  <footer className="main-footer" id="contact-main">
+    <section className="footer-content">
+      <h2>Conact</h2>
 
-            <div className="footer-sect-wrapper">
-                <article className="contact-info">
-                    <div>
-                        <h3>Adress</h3>
-                        <List items={['some street 10/25']}/>
-                    </div>
-                    <div>
-                        <h3>Open hours</h3>
-                        <List items={['Mon - Fir: 8:00am - 3pm','Sat - Sun: 8:00am - 11am']}/>
-                    </div>
-                    <div>
-                        <h3>Contact us</h3>
-                        <List items={['email: makeastory@gmail.com','phone: 503 294 492']}/>
-                    </div>
-                    <div>
-                        <h3>FAQ</h3>
-                        <List items={[
-                            "What's the cost of delivery",
-                            'How to buy a book',
-                            'Whre i can find the sales'
-                        ]}/>
-                    </div>
-                </article>
-                <form className="footer-form">
-                    <input placeholder="Name"/>
-                    <input placeholder="Email"/>
-                    <textarea placeholder="Message" rows="5"/>
-                    <div className="footer-submit">
-                        <Button secondary>Send</Button>
-                    </div>
-                </form>
-            </div>
-            <div className="footer-bootom">
-                <p>&copy; Make a story 2020</p>
-            </div>
-        </section>
-    </footer>
-)
+      <div className="footer-sect-wrapper">
+        <article className="contact-info">
+          <div>
+            <h3>Adress</h3>
+            <List items={["some street 10/25"]} />
+          </div>
+          <div>
+            <h3>Open hours</h3>
+            <List
+              items={["Mon - Fir: 8:00am - 3pm", "Sat - Sun: 8:00am - 11am"]}
+            />
+          </div>
+          <div>
+            <h3>Contact us</h3>
+            <List
+              items={["email: makeastory@gmail.com", "phone: 503 294 492"]}
+            />
+          </div>
+          <div>
+            <h3>FAQ</h3>
+            <List
+              items={[
+                "What's the cost of delivery",
+                "How to buy a book",
+                "Whre i can find the sales",
+              ]}
+            />
+          </div>
+        </article>
+        <form className="footer-form">
+          <label htmlFor="name-control" className="input-label">
+            Name
+          </label>
+          <input placeholder="Name" id="name-control" />
+          <label htmlFor="email-control" className="input-label">
+            Email
+          </label>
+          <input placeholder="Email" id="email-control" />
+          <label htmlFor="message-control" className="input-label">
+            Message
+          </label>
+          <textarea placeholder="Message" rows="5" id="message-control" />
+          <div className="footer-submit">
+            <Button secondary>Send</Button>
+          </div>
+        </form>
+      </div>
+      <div className="footer-bootom">
+        <p>&copy; Make a story 2020</p>
+      </div>
+    </section>
+  </footer>
+);
 
 export default Footer;
--- src/pages/product/product.js
+++ src/pages/product/product.js
@@ -1,64 +1,68 @@
-import React from 'react';
-import './product.css';
-import Box from '../../components/box/box';
-import { Link } from 'react-router-dom';
-import Button from '../../components/button/button';
+import React from "react";
+import "./product.css";
+import Box from "../../components/box/box";
+import { Link } from "react-router-dom";
+import Button from "../../components/button/button";
 
 const Product = () => {
-    return (
-        <div className="product-wrapper">
-            <section className="product-item">
-                <div className="hp-product-wrapper-i">
-                    <Box>
-                        <div className="hp-product-i">
-                            <div className="hp-img-wrapper-i">
-                                <figure className="hp-product-img-i">
-                                    <img src={process.env.PUBLIC_URL + '/book-mockup.png'} alt="book" />
-                                </figure>
-                            </div>
-                            <section className="hp-get-book-i">
-
-                                <h3>The book</h3>
-                                <p className="hp-price-i">19.99$</p>
-                                <div className="hp-get-btn-i">
-                                    <Link to="/product">
-                                        <Button>Buy</Button>
-                                    </Link>
-                                </div>
-                            </section>
-                        </div>
-                    </Box>
+  return (
+    <div className="product-wrapper">
+      <section className="product-item">
+        <div className="hp-product-wrapper-i">
+          <Box>
+            <div className="hp-product-i">
+              <div className="hp-img-wrapper-i">
+                <figure className="hp-product-img-i">
+                  <img
+                    src={process.env.PUBLIC_URL + "/book-mockup.webp"}
+                    alt="book"
+                  />
+                </figure>
+              </div>
+              <section className="hp-get-book-i">
+                <h3>The book</h3>
+                <p className="hp-price-i">19.99$</p>
+                <div className="hp-get-btn-i">
+                  <Link to="/product">
+                    <Button>Buy</Button>
+                  </Link>
                 </div>
-            </section>
-            <article className="desc-wrapper"> 
-                <section className="tile-text-i">
-                    <h2>Description</h2>
-                    <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Natus, excepturi aut error nemo eveniet expedita dolorum,
-                        quod culpa explicabo rem quasi vitae corporis ut debitis,
-                        tempora distinctio. Explicabo corrupti doloribus,
-                        cupiditate exercitationem voluptatum quod
-                        quod culpa explicabo rem quasi vitae corporis ut debitis,
-                        tempora distinctio. Explicabo corrupti doloribus,
-                        cupiditate exercitationem voluptatum quod
-                    </p>
-                </section>
-                <section className="tile-bg-1-i tile-i">
-                    <div className="tile-img-wrapper-i">
-                        <Box>
-                            <div className="tile-image-i">
-                                <img src={process.env.PUBLIC_URL + '/read.jpeg'} alt="tile-img"/>
-                            </div>
-                        </Box>
-                    </div>
-                    <div className="tile-1-btn-wrapper-i">
-                        <Button>Read More</Button>
-                    </div>
-                </section>
-            </article>
+              </section>
+            </div>
+          </Box>
         </div>
-    )
-}
+      </section>
+      <article className="desc-wrapper">
+        <section className="tile-text-i">
+          <h2>Description</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus,
+            excepturi aut error nemo eveniet expedita dolorum, quod culpa
+            explicabo rem quasi vitae corporis ut debitis, tempora distinctio.
+            Explicabo corrupti doloribus, cupiditate exercitationem voluptatum
+            quod quod culpa explicabo rem quasi vitae corporis ut debitis,
+            tempora distinctio. Explicabo corrupti doloribus, cupiditate
+            exercitationem voluptatum quod
+          </p>
+        </section>
+        <section className="tile-bg-1-i tile-i">
+          <div className="tile-img-wrapper-i">
+            <Box>
+              <div className="tile-image-i">
+                <img
+                  src={process.env.PUBLIC_URL + "/read.jpeg"}
+                  alt="tile-img"
+                />
+              </div>
+            </Box>
+          </div>
+          <div className="tile-1-btn-wrapper-i">
+            <Button>Read More</Button>
+          </div>
+        </section>
+      </article>
+    </div>
+  );
+};
 
 export default Product;
